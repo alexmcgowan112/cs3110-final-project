a3d19c64b081f421b409f5b4c9b7dd3d5a61ea55
August Ehrlich <ae427@cornell.edu>
2025-03-29 20:16:13 -0400

    Removed the E key binding
     - It wasn't useful for anything but testing and had issues with reading input.

--- lib/keyboard.mli
+++ lib/keyboard.mli
@@ -1,3 +1,4 @@
+(** The type [t] represents keyboard inputs. *)
 type t =
   | ArrowUp
   | ArrowDown
@@ -7,11 +8,18 @@ type t =
   | Q
   | None
 
+(** Functor [MakeInput] creates a module for handling keyboard input. *)
 module MakeInput : functor
   (G : sig
      val get : unit -> Curses.attr_t
+     (** [get] retrieves the current attribute of the terminal. *)
    end)
   -> sig
   val read_input : unit -> t
+  (** [read_input] reads a keyboard input and returns its corresponding type
+      [t]. *)
+
   val string_of_input : t -> string
+  (** [string_of_input] converts a keyboard input of type [t] to its string
+      representation. *)
 end
--- test/test_cs3110_final_project.ml
+++ test/test_cs3110_final_project.ml
@@ -2,13 +2,18 @@ open OUnit2
 open Cs3110_final_project
 
 module Input = Keyboard.MakeInput (struct
+  (** [arr] is an array of integers representing a sequence of key codes. *)
   let arr =
     [|
       98; 113; Curses.Key.up; Curses.Key.down; Curses.Key.left; Curses.Key.right;
     |]
-
+  (** [ind] is a reference to an integer that tracks the current index in the
+      [arr] array. *)
   let ind = ref 0
-
+  (** [get ()] retrieves the current key code from the [arr] array based on the
+      index stored in [ind]. If the index is within bounds, it increments [ind]
+      and returns the key code at the current index. If the index is out of
+      bounds, it returns [1]. *)
   let get () =
     let i = !ind in
     if i < Array.length arr then (
@@ -18,6 +23,18 @@ module Input = Keyboard.MakeInput (struct
     else 1
 end)
 
+(** [string_of_list elem_to_string lst] converts a list [lst] into its string
+    representation. Each element of the list is converted to a string using the
+    function [elem_to_string], and the elements are separated by semicolons
+    within square brackets.
+
+    Example:
+    - [string_of_list string_of_int [1; 2; 3]] evaluates to "[1;2;3;]".
+
+    @param elem_to_string
+      A function that converts an element of the list to a string.
+    @param lst The list to be converted to a string.
+    @return A string representation of the list. *)
 let string_of_list elem_to_string lst =
   "[" ^ List.fold_left (fun acc el -> elem_to_string el ^ ";" ^ acc) "]" lst
 
