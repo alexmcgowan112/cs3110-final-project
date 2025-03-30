ed27e4cd07e0cd6ca7f766a27a325158ca5028a6
August Ehrlich <ae427@cornell.edu>
2025-03-30 19:28:57 -0400

    Adds tests for the coords compilation unit.
     - also adds one test for get player position

--- test/test_cs3110_final_project.ml
+++ test/test_cs3110_final_project.ml
@@ -40,6 +40,12 @@ let tests =
              (Room.get_player_pos
                 (run_inputs [ Keyboard.Right; Keyboard.Right ]))
              ~cmp:Coords.equal ~printer:Coords.to_string );
+         ( "getting player position at the start is equal to the starting \
+            position"
+         >:: fun _ ->
+           assert_equal
+             (Room.get_player_pos (run_inputs []))
+             { x = 5; y = 5 } ~cmp:Coords.equal ~printer:Coords.to_string );
          ( "running the bomb fuse out explodes it" >:: fun _ ->
            assert_bool "No explosion occuring"
              (Room.exploding
@@ -78,6 +84,28 @@ let tests =
            let exp = Explosion.create { x = 5; y = 5 } 0 in
            Explosion.spread exp;
            assert_bool "In progress" (not (Explosion.is_in_progress exp)) );
+         ( "coordinates equal in both elements are equal" >:: fun _ ->
+           assert_bool "Not equal"
+             (Coords.equal { x = 5; y = 5 } { x = 5; y = 5 }) );
+         ( "coordinates not equal in either element are not equal" >:: fun _ ->
+           assert_bool "Equal"
+             (not (Coords.equal { x = 5; y = 5 } { x = 6; y = 5 }));
+           assert_bool "Equal"
+             (not (Coords.equal { x = 5; y = 5 } { x = 5; y = 6 })) );
+         ( "adding two coordinates is a coordinate with the elements summed"
+         >:: fun _ ->
+           assert_equal
+             (Coords.add { x = 5; y = 5 } { x = 6; y = 6 })
+             { x = 11; y = 11 } ~cmp:Coords.equal ~printer:Coords.to_string );
+         ( "Euclidean distance between two coordinates works" >:: fun _ ->
+           assert_equal
+             ~cmp:(cmp_float ~epsilon:0.0001)
+             (Coords.euclid_dist { x = 5; y = 5 } { x = 6; y = 6 })
+             1.4142135623730951 ~printer:string_of_float );
+         ( "Manhattan distance between two coordinates works" >:: fun _ ->
+           assert_equal
+             (Coords.manhattan_dist { x = 5; y = 5 } { x = 6; y = 6 })
+             2 ~cmp:Int.equal ~printer:string_of_int );
        ]
 
 let _ = run_test_tt_main tests
