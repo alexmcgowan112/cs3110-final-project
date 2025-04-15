740ac5a4732c085d2f32271f9d03dcc2fbdaca2a
ilomacht <104243469+ilomacht@users.noreply.github.com>
2025-04-15 11:02:49 -0400

    Implement placeholder enemy class with Coords.t

--- lib/enemies.ml
+++ lib/enemies.ml
@@ -1,8 +1,34 @@
-type t = unit (* You can change this later to a real type *)
+type t = {
+  id : int;
+  mutable position : Coords.t;
+  mutable health : int;
+}
 
-let create _ _ = failwith "Not implemented"
-let move _ _ _ = failwith "Not implemented"
-let get_position _ = failwith "Not implemented"
-let next_move _ _ = failwith "Not implemented"
-let take_damage _ _ = failwith "Not implemented"
-let is_alive _ = failwith "Not implemented"
+let create coords id = { id; position = coords; health = 100 }
+let move this coords = this.position <- coords
+let get_position this = this.position
+
+let next_move room this =
+  (* Placeholder logic for next move. Replace an A* closest path algorithm. This
+     does not yet guard against walls lol*)
+  let target = Room.get_player_pos room in
+  let dx =
+    if target.Coords.x > this.position.Coords.x then 1
+    else if target.Coords.x < this.position.Coords.x then -1
+    else 0
+  in
+  let dy =
+    if target.Coords.y > this.position.Coords.y then 1
+    else if target.Coords.y < this.position.Coords.y then -1
+    else 0
+  in
+  {
+    Coords.x = this.position.Coords.x + dx;
+    Coords.y = this.position.Coords.y + dy;
+  }
+
+let take_damage this damage =
+  this.health <- this.health - damage;
+  if this.health < 0 then this.health <- 0
+
+let is_alive this = this.health > 0
--- lib/enemies.mli
+++ lib/enemies.mli
@@ -1,11 +1,14 @@
 type t
 
-val create : int -> int -> t
-(**xposition->yposition->enemy*)
-val move : t -> int -> int -> unit
+val create : Coords.t -> int -> t
+(**xposition->yposition->id->enemy*)
+val move : t -> Coords.t -> unit
 (**move enemy to xposition yposition*)
-val get_position : t -> int * int
+val get_position : t -> Coords.t
 (**get enemy position*)
-val next_move : Room.t -> t -> int * int
+val next_move : Room.t -> t -> Coords.t
+(**gets the next move of the enemy, this will be called in room (maybe?)*)
 val take_damage : t -> int -> unit
+(**takes damage from player/bomb etc.*)
 val is_alive : t -> bool
+(**is alive, removed from enemies list if false*)
