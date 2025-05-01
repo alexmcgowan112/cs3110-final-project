193aeda2e43f17ad5add4098d3a110547385c4ef
Alex McGowan <acm355@cornell.edu>
2025-04-30 23:05:23 -0400

    Explosion fix

--- lib/basics/game.ml
+++ lib/basics/game.ml
@@ -77,7 +77,6 @@ let hud_text dungeon =
 let process_world dungeon =
   (*main game loop*)
   let cmd_palette_display = handle_input dungeon in
-  Room.explode (Dungeon.current_room dungeon);
   Room.update_items (Dungeon.current_room dungeon) (Dungeon.player dungeon);
   Room.update_enemies (Dungeon.current_room dungeon) (Dungeon.player dungeon);
   Room.explode (Dungeon.current_room dungeon);
